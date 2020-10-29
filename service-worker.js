2ec162bbb40df713ef47dba68ec6c0570cddde3c
Ivan Beletskiy <beletsky02@gmail.com>
2020-10-29 12:53:43 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.7fa90f2dc5745015d8a6ef55f8f3a116.js"
 );
 
 self.addEventListener('message', (event) => {
